8cc30ef1ff31c1a9776e064cf02ac82075f08517
Helvio Multiconecta <helvio@multiconecta.com.br>
2019-02-22 16:11:40 -0300

    Added pause button with pausing and restart funtions

--- ajax/timer.php
+++ ajax/timer.php
@@ -12,79 +12,128 @@ if (isset($_POST["action"])) {
       case 'start':
          if (PluginActualtimeTask::checkTimerActive($task_id)) {
             $result=[
-               'mensage'=>__("A user is already performing the task", 'actualtime'),
-               'title' => __s('Warning'),
-               'class' => 'warn_msg',
+               'mensage' => __("A user is already performing the task", 'actualtime'),
+               'title'   => __('Warning'),
+               'class'   => 'warn_msg',
             ];
          } else {
-            if (PluginActualtimeTask::checkUserFree(Session::getLoginUserID())) {
+            if (! PluginActualtimeTask::checkUserFree(Session::getLoginUserID())) {
                $opcional=PluginActualtimeTask::getTicket(Session::getLoginUserID());
                $result=[
-                  'mensage'=>__("You are already doing a task", 'actualtime')." <a href='/front/ticket.form.php?id=".$opcional."'>".__("Ticket")."</a>",
-                  'title' => __s('Warning'),
-                  'class' => 'warn_msg',
+                  'mensage' => __("You are already doing a task", 'actualtime')." <a href='/front/ticket.form.php?id=".$opcional."'>".__("Ticket")."&forcetab=Ticket%241</a>",
+                  'title'   => __('Warning'),
+                  'class'   => 'warn_msg',
                ];
             } else {
-               $DB->insert(
-                  'glpi_plugin_actualtime_tasks', [
-                     'tasks_id'      => $task_id,
-                     'actual_begin' => date("Y-m-d H:i:s"),
-                     'users_id'=>Session::getLoginUserID(),
-                  ]
-               );
-               $result=[
-                  'mensage'=>__("Timer started", 'actualtime'),
-                  'title' => __s('Information'),
-                  'class' => 'info_msg',
-               ];
+               if (! PluginActualtimeTask::checkTech($task_id)) {
+                  // covering all bases, buttons would not show anyway
+                  $result=[
+                     'mensage' => __("You are not assigned to this task", 'actualtime'),
+                     'title'   => __('Warning'),
+                     'class'   => 'warn_msg',
+                  ];
+               } else {
+                  $DB->insert(
+                     'glpi_plugin_actualtime_tasks', [
+                        'tasks_id'     => $task_id,
+                        'actual_begin' => date("Y-m-d H:i:s"),
+                        'users_id'     => Session::getLoginUserID(),
+                     ]
+                  );
+                  $result=[
+                     'mensage' => __("Timer started", 'actualtime'),
+                     'title'   => __('Information'),
+                     'class'   => 'info_msg',
+                  ];
+               }
             }
          }
          echo json_encode($result);
          break;
 
       case 'end':
+      case 'pause':
          if (PluginActualtimeTask::checkTimerActive($task_id)) {
+
             if (PluginActualtimeTask::checkUser($task_id, Session::getLoginUserID())) {
+
                $actual_begin=PluginActualtimeTask::getActualBegin($task_id);
                $seconds=(strtotime(date("Y-m-d H:i:s"))-strtotime($actual_begin));
                $DB->update(
                   'glpi_plugin_actualtime_tasks', [
-                     'actual_end'      => date("Y-m-d H:i:s"),
-                     'actual_actiontime'      => $seconds,
+                     'actual_end'        => date("Y-m-d H:i:s"),
+                     'actual_actiontime' => $seconds,
                   ], [
-                     'tasks_id'=>$task_id,
+                     'tasks_id' => $task_id,
                      [
                         'NOT' => ['actual_begin' => null],
                      ],
-                     'actual_end'=>null,
+                     'actual_end' => null,
                   ]
                );
-               $DB->update(
-                  'glpi_tickettasks', [
-                     'state'=>2,
-                  ], [
-                     'id'=>$task_id,
-                  ]
-               );
+               if ($_POST['action']=='end') {
+                  $DB->update(
+                     'glpi_tickettasks', [
+                        'state' => 2,
+                     ], [
+                        'id' => $task_id,
+                     ]
+                  );
+               }
+               $result=[
+                  'mensage' => __("Timer completed", 'actualtime'),
+                  'title'   => __('Information'),
+                  'class'   => 'info_msg',
+               ];
+
+            } else {
 
                $result=[
-                  'mensage'=>__("Timer completed", 'actualtime'),
-                  'title' => __s('Information'),
-                  'class' => 'info_msg',
+                  'mensage' => __("Only the user who initiated the task can close it", 'actualtime'),
+                  'title'   => __('Warning'),
+                  'class'   => 'warn_msg',
+               ];
+
+            }
+
+         } else {
+
+            if ($_POST['action']=='pause') {
+               $result=[
+                  'mensage' => __("The task had not been initialized", 'actualtime'),
+                  'title'   => __('Warning'),
+                  'class'   => 'warn_msg',
                ];
             } else {
-               $result=[
-                  'mensage'=>__("Only the user who initiated the task can close it", 'actualtime'),
-                  'title' => __s('Warning'),
-                  'class' => 'warn_msg',
-               ];
+
+               if (PluginActualtimeTask::checkTech($task_id)) {
+
+                  if ($_POST["action"]=='end') {
+
+                     $DB->update(
+                        'glpi_tickettasks', [
+                           'state' => 2,
+                        ], [
+                           'id' => $task_id,
+                        ]
+                     );
+                     $result=[
+                        'mensage' => __("Task completed."),
+                        'title'   => __('Information'),
+                        'class'   => 'info_msg',
+                     ];
+
+                  } else {
+
+                     $result=[
+                        'mensage' => __("You are not assigned to this task", 'actualtime'),
+                        'title'   => __('Warning'),
+                        'class'   => 'warn_msg',
+                     ];
+
+                  }
+               }
             }
-         } else {
-            $result=[
-               'mensage'=>__("The task had not been initialized", 'actualtime'),
-               'title' => __s('Warning'),
-               'class' => 'warn_msg',
-            ];
          }
          echo json_encode($result);
          break;
