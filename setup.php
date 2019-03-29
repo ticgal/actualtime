a6abe2a85cba5a86f3d1850ecedb90a889429d0d
Xacobo Freire <39123808+xacobofg@users.noreply.github.com>
2019-03-29 18:05:01 +0100

    Change version 1.2.0

--- setup.php
+++ setup.php
@@ -1,5 +1,5 @@
 <?php
-define ('PLUGIN_ACTUALTIME_VERSION', '1.1.3');
+define ('PLUGIN_ACTUALTIME_VERSION', '1.2.0');
 // Minimal GLPI version, inclusive
 define("PLUGIN_ACTUALTIME_MIN_GLPI", "9.3.0");
 // Maximum GLPI version, exclusive
