23de7044428fcea13b08bdf61a4e63aeab243253
Xacobo Freire <xacobo@ticgal.com>
2022-07-26 13:57:25 +0200

    Show time in closed task

--- inc/task.class.php
+++ inc/task.class.php
@@ -815,13 +815,14 @@ JAVASCRIPT;
                $timercolor = (self::checkTimerActive($task_id) ? 'red' : 'black');
                // Anchor to find correct span, even when user has no update
                // right on status checkbox
+               $icon = "<span class='badge text-wrap ms-1 d-none d-md-block' style='color:{$timercolor}'><i id='actualtime_faclock_{$task_id}_{$rand}' class='fa{$fa_icon}'></i> <span id='actualtime_timer_{$task_id}_box_{$rand}'></span></span>";
                echo "<div id='actualtime_anchor_{$task_id}_{$rand}'></div>";
                $script = <<<JAVASCRIPT
 $(document).ready(function() {
    if ($("[id^='actualtime_faclock_{$task_id}_']").length == 0) {
-      $("#viewitemtickettask{$task_id} div.displayed_content div.item_content p span.state")
-         .after("<i id='actualtime_faclock_{$task_id}_{$rand}' class='fa{$fa_icon}' style='color:{$timercolor}; padding:3px; vertical-align:middle;'></i><span id='actualtime_timer_{$task_id}_box_{$rand}' style='color:{$timercolor}; vertical-align:middle;'></span>");
-      if ($time > 0) {
+      $("div[data-itemtype='TicketTask'][data-items-id='{$task_id}'] div.card-body div.timeline-header div.creator")
+         .append("{$icon}");
+         if ($time > 0) {
          window.actualTime.fillCurrentTime($task_id, $time);
       }
    }
