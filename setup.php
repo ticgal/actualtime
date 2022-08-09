c213030bce9b7091d4d3123f98e71051ddffaf2b
Xacobo Freire <39123808+xacobofg@users.noreply.github.com>
2022-08-09 14:34:51 +0200

    Change version 2.0.0-beta.3
    Signed-off-by: Xacobo Freire <39123808+xacobofg@users.noreply.github.com>

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  ----------------------------------------------------------------------
 */
 
-define('PLUGIN_ACTUALTIME_VERSION', '2.0.0-beta.2');
+define('PLUGIN_ACTUALTIME_VERSION', '2.0.0-beta.3');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ACTUALTIME_MIN_GLPI", "10.0.0");
