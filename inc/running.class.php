827dba9cb53e0b9592ebf468c4ad6252f12cf392
Aarón <aaron@ticgal.com>
2026-01-19 11:01:33 +0100

    glpi 11

--- inc/running.class.php
+++ inc/running.class.php
@@ -97,7 +97,7 @@ class PluginActualtimeRunning extends CommonGLPI
             function loadRunning(){
                 $.ajax({
                     type:'POST',
-                    url:CFG_GLPI.root_doc+"/"+GLPI_PLUGINS_PATH.actualtime+"/ajax/running.php",
+                    url:CFG_GLPI.root_doc+"/plugin/actualtime/ajax/running.php",
                     data:{
                         action:'getlist'
                     },
--- setup.php
+++ setup.php
@@ -36,7 +36,7 @@ define('PLUGIN_ACTUALTIME_VERSION', '3.2.3');
 // Minimal GLPI version, inclusive
 define("PLUGIN_ACTUALTIME_MIN_GLPI", "10.0.10");
 // Maximum GLPI version, exclusive
-define("PLUGIN_ACTUALTIME_MAX_GLPI", "10.1.0");
+define("PLUGIN_ACTUALTIME_MAX_GLPI", "11.1.0");
 define("PLUGIN_ACTUALTIME_NAME", "ActualTime");
 
 /**
@@ -71,7 +71,6 @@ function plugin_init_actualtime(): void
     /** @var array $PLUGIN_HOOKS */
     global $PLUGIN_HOOKS;
 
-    $PLUGIN_HOOKS[Hooks::CSRF_COMPLIANT]['actualtime'] = true;
 
     if (Plugin::isPluginActive('actualtime')) {
         // Classes
@@ -138,13 +137,13 @@ function plugin_init_actualtime(): void
         }
 
         if (Session::getLoginUserID()) {
-            $PLUGIN_HOOKS[Hooks::ADD_JAVASCRIPT]['actualtime'] = 'js/actualtime.js';
 
             /** @var array $CFG_GLPI */
             global $CFG_GLPI;
 
             // preload the fullcalendar library
             $CFG_GLPI['javascript']['actualtime']['sourcetimer'] = ['fullcalendar'];
+            $PLUGIN_HOOKS[Hooks::ADD_JAVASCRIPT]['actualtime'] = 'public/actualtime.js';
         }
 
         if (Session::haveRight('plugin_actualtime_running', READ)) {
