e90c36185a2e8d1c58e76ed6a8c7e50837292cd5
Aarón <aaron@ticgal.com>
2026-01-22 10:35:21 +0100

    update version

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_ACTUALTIME_VERSION', '4.0.0-beta.2');
+define('PLUGIN_ACTUALTIME_VERSION', '4.0.0-beta.3');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ACTUALTIME_MIN_GLPI", "11.0.0");
