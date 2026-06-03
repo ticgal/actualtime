5545c546d0c324333f82b7718ce65c9b5dcdb259
Xacobo Freire <xacobo@ticgal.com>
2026-06-03 08:20:49 +0200

    change version 3.2.5

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_ACTUALTIME_VERSION', '3.2.5-beta');
+define('PLUGIN_ACTUALTIME_VERSION', '3.2.5');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ACTUALTIME_MIN_GLPI", "10.0.10");
