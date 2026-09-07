09c7b78b99683579e686e8f94ac267d5e5f9209e
María García <maria.garcia@ticgal.com>
2026-09-07 09:20:24 +0200

    fix usage graph

--- inc/sourcetimer.class.php
+++ inc/sourcetimer.class.php
@@ -354,4 +354,11 @@ JAVASCRIPT;
             $DB->doQuery($query);
         }
     }
+
+    public static function uninstall(Migration $migration): void
+    {
+        $table = self::getTable();
+        $migration->displayMessage("Uninstalling $table");
+        $migration->dropTable($table);
+    }
 }
